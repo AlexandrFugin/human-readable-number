be9f675299121360805e56f3d7c4d242c057630d
AlexandrFugin <fav2.7@mail.ru>
2022-07-18 12:13:02 +0300

    fix: partially completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function toReadable (number) {
-  
+    let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve' , 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hundreds = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+
+  if (number == 0) {
+    return 'zero';
+  } 
+  if (number > 0 && number <= 19) {
+    return (units[number]);
+  } 
+  if (number > 19 && number <= 99 && number % 10 != 0) {
+    let a = (dozens[number.toString()[0]] + ' ' + units[number.toString()[1]]);
+    console.log(a);
+    return a;
+  } 
+  else if (number > 19 && number <= 99) {
+    return (dozens[number.toString()[0]]); 
+  } 
+
+     if (number > 99 && number <= 999) {
+      if (number % 100 != 0) {
+        let o = hundreds[number.toString()[0]] + ' ' + dozens[number.toString()[1]] + ' ' + units[number.toString()[2]];
+        console.log(o.trim());
+        return o.trim();
+      } 
+      else if (Math.trunc(number % 100 / 10) == 1) {
+          // console.log(number[1]);
+          let y = hundreds[number.toString()[0]] + ' ' + dozens[number.toString()[1]];
+        return y;
+      } 
+      else if (Math.trunc(number % 100 / 10) == 0) {
+        let q = hundreds[number.toString()[0]] + ' ' + units[number.toString()[2]];
+        return q;
+      }
+    }
+
+  // else {
+  //   return (0);
+  // }
 }
